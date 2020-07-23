e168113c68f89d26214c3b01d635a0ff535a2e45
Daniel Paz <danielpaz601@gmail.com>
2020-07-23 13:21:37 +0300

    Added sounds

---
+++ Assets/Scripts/TimeScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.right, 10 * Time.deltaTime);
+		transform.LookAt(Vector3.zero);
+    }
+}
