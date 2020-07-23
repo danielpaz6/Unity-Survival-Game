f8b04a34f86e5d0602565a5f1d1e05f36828062f
MayYosef <mayosef25@gmail.com>
2020-07-23 18:23:26 +0300

    add menuScene & fonts

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+	public Button playText;
+	public Button exitText;
+
+	public void StartLevel()
+	{
+		// The main menu scene is 0
+		Debug.Log("start1");
+		SceneManager.LoadScene(1);
+		Debug.Log("start2");
+	}
+
+	public void ExitGame()
+	{
+		Debug.Log("exit1");
+		Application.Quit();
+		Debug.Log("exit2");
+	}
+}
