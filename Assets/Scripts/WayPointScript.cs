c1e7a7862b8cea35f75a552cedc167ce2aa86bef
Daniel Paz <danielpaz601@gmail.com>
2020-07-26 12:54:28 +0300

    Added new scene and patrol feature

---
+++ Assets/Scripts/WayPointScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointScript : MonoBehaviour
+{
+	public Transform[] waypoint;
+	public float speed = 5;
+	int currentWayPoint;
+	Vector3 target, moveDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = waypoint[currentWayPoint].position;
+		moveDirection = target - transform.position;
+
+		if(moveDirection.magnitude < 3) {
+			currentWayPoint = ++currentWayPoint % waypoint.Length;
+		}
+
+		GetComponent<Rigidbody>().velocity = moveDirection.normalized * speed;
+		
+		//Vector3 newDirection = Vector3.RotateTowards(transform.forward, moveDirection, 0.0f, 0.0f);
+		//transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+}
