1dcc1fffd4379f76a31c958957b10feb7c9fc307
MayYosef <mayosef25@gmail.com>
2020-08-04 12:10:23 +0300

    deep learning

---
+++ Assets/Scripts/SwitchScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScript : MonoBehaviour
+{
+	public GameObject boat;
+	public GameObject boatCamera;
+	public GameObject person;
+	public GameObject playerStartPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+		// out of boat
+        if(Input.GetKey("1")) {
+			boat.GetComponent<Rigidbody>().isKinematic = false;
+			boat.GetComponent<BoatScript>().enabled = true;
+			boatCamera.SetActive(true);
+			person.SetActive(false);
+		}
+		else if(Input.GetKey("2")) {
+			boat.GetComponent<Rigidbody>().isKinematic = true;
+			boat.GetComponent<BoatScript>().enabled = false;
+			boatCamera.SetActive(false);
+			person.SetActive(true);
+			person.transform.position = playerStartPosition.transform.position;
+		}
+    }
+}
