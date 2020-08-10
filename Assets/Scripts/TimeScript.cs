7dfe8c3f82d7bf510153bd8813eea247721cd19d
Daniel Paz <danielpaz601@gmail.com>
2020-08-10 12:06:43 +0300

    minor fixes

--- Assets/Scripts/TimeScript.cs
+++ Assets/Scripts/TimeScript.cs
@@ -7,7 +7,7 @@ public class TimeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(Vector3.zero, Vector3.right, 0.5f * Time.deltaTime);
-		transform.LookAt(Vector3.zero);
+        //transform.RotateAround(Vector3.zero, Vector3.right, 0.5f * Time.deltaTime);
+		//transform.LookAt(Vector3.zero);
     }
 }
